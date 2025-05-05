ffea8dcc452702fd422db37548edf904f6ac0441
Phuchit <phuchit.u@gmail.com>
2025-05-05 21:05:56 +0700

    Add test steps to make two rooms for compare the behavior

--- spec/system/user_can_add_room_with_other_user_spec.rb
+++ spec/system/user_can_add_room_with_other_user_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 describe "Room creation with participants" do
   context "when user creates a room and adds another user" do
-    it "allows the added user to access the added room", js: true do
+    it "allows the added user to access only the room they were added to", js: true do
       given_two_users_exist
       login_as_creator
       user_creator_creates_room_with_select_another_user
+      user_creator_creates_room_without_another_user
       creator_should_see_room_created
       logout_creator
       login_as_added_user
-      added_user_should_see_the_room
+      added_user_should_see_only_first_room
     end
   end
 
@@ -24,13 +25,21 @@ describe "Room creation with participants" do
   end
 
   def user_creator_creates_room_with_select_another_user
-    fill_in 'room_name', with: 'Test Room'
+    fill_in 'room_name', with: 'Room 1'
     find("input[type='checkbox'][value='#{@added_user.id}']").set(true)
     click_button 'Create Room'
   end
 
+  def user_creator_creates_room_without_another_user
+    visit root_path
+    fill_in 'room_name', with: 'Room 2'
+    click_button 'Create Room'
+  end
+
   def creator_should_see_room_created
-    expect(page).to have_content('Test Room')
+    visit root_path
+    expect(page).to have_content('Room 1')
+    expect(page).to have_content('Room 2')
   end
 
   def logout_creator
@@ -44,7 +53,8 @@ describe "Room creation with participants" do
     visit root_path
   end
 
-  def added_user_should_see_the_room
-    expect(page).to have_content('Test Room')
+  def added_user_should_see_only_first_room
+    expect(page).to have_content('Room 1')
+    expect(page).not_to have_content('Room 2')
   end
 end
